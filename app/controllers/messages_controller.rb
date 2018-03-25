680db5ab8500f95eb48e95c76419dcf69037a3fd
y-sagawa <y.s@Mac.local>
2018-03-25 14:20:33 +0900

    Edit messages_controller & message.scss

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,27 +1,31 @@
 class MessagesController < ApplicationController
-  before_action :set_group
+  before_action :set_group, :set_messages
 
   def index
     @message = Message.new
-    @messages = @group.messages.includes(:user)
   end
 
   def create
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.html{ redirect_to group_messages_path }
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
-      flash.now[:alert] = 'メッセージを入力してください。'
       render :index
     end
   end
-
 
   private
 
   def message_params
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
+  def set_messages
+    @messages = @group.messages.includes(:user)
   end
 
   def set_group
