28b696e8b06249130331f258f585c0333d7cc212
y-sagawa <y.s@Mac.local>
2018-03-25 14:14:44 +0900

    Correct Group-Edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
-  resource :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
-    resources :messages, only: [:index, :create]
+    resources :messages, only: [:index, :create, :edit, :update]
   end
 end
 
