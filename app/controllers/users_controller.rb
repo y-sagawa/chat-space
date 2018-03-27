8e18ca26d83e505284bb929bfb8ee9003b4bb959
y-sagawa <y.s@Mac.local>
2018-03-27 10:48:08 +0900

    Add to routes.rb & users_controller.rb (index)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
-  def show
+  def index
+    @users = User.where("name LIKE(?)", "%#{params[:keyword]}%")
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   def edit
