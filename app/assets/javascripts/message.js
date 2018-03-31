d5d60ddcfd6677adb96598318220eb9a327aad5e
y-sagawa <y.s@Mac.local>
2018-03-31 15:12:34 +0900

    Add autoupdate function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,39 @@
 $(function(){
+
+  function autoupdate(){
+  var message_id = $('.list').last().data('message-id');
+  console.log(message_id)
+  if(window.location.href.match(/\/groups\/\d+\/messages/)){
+    if(fail)return false;
+    $.ajax({
+      url: location.href,
+      type : 'GET',
+      data : { message: {id: message_id} },
+      dataType: 'json'
+    })
+
+    .done(function(last){
+      var insertHTML = '';
+      last.messages.forEach(function(message){
+            insertHTML += buildHTML(message);
+            $(".main-middle-content").append(insertHTML);
+            $(".main-middle-content").animate({scrollTop:$(".main-middle-content")[0].scrollHeight}, "swing");
+        return false;
+      });
+    })
+    .fail(function(last){
+      alert('自動更新に失敗しました');
+      fail = true;
+    });
+  }
+ }
+
   function buildHTML(message){
     if (message.image.url == null ){
       message.image.url = ''
     }
     var html =
-    `<div class = "main-middle-content">
-      <ul class = "list">
+    `<ul class = "list" data-message-id= "${message.id}">
         <li class = "list__name">
           <p>
             ${message.user_name}
@@ -22,10 +50,15 @@ $(function(){
           </p>
           <img src= "${message.image.url}"/, class = "list__message__image" >
         </li>
-      </ul>
-    </div>`
+      </ul>`
     return html;
+    console.log(message)
   }
+fail = ""
+  setInterval(autoupdate, 5000);
+
+
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -37,19 +70,20 @@ $(function(){
       data: formData,
       dataType: 'json',
       processData: false,
-      contentType: false
+      contentType: false,
     })
     .done(function(data){
       var html = buildHTML(data);
-      $('.main-middle-content').append(html)
-      $('#message-content').val('')
-      $('#message-image.url').val('')
-      $(".submit").prop("disabled", false)
-      $(".main-middle-content").animate({scrollTop:$(".main-middle-content")[0].scrollHeight}, 500, "swing");
+      $('.main-middle-content').append(html);
+      $('#message_content').val('');
+      $('#message_image').val('');
+      $(".form_submit").prop('disabled', false);
+      $(".main-middle-content").animate({scrollTop:$(".main-middle-content")[0].scrollHeight}, "swing");
       alert('送信が完了しました')
     })
     .fail(function(){
       alert('失敗しました');
     })
-  })
+    return false;
+  });
 });
