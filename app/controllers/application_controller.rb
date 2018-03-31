0ddcddcd3270941eed3cfe14468441e8fe5565f2
y-sagawa <y.s@Mac.local>
2018-03-31 14:48:51 +0900

    Correct sign-in,sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  protect_from_forgery prepend: true
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def after_sign_out_path_for(resource)
+   new_user_session_path
   end
 end
